e31bd30141b113af6e416e3201f2b6276d836e92
lbj <951744095@qq.com>
2022-06-29 18:23:00 +0800

    removeNthFromEnd4

--- src/linkedList/removeNthFromEnd.java
+++ src/linkedList/removeNthFromEnd.java
@@ -6,22 +6,26 @@ package linkedList;
  * 2. 需要删除倒数指定的结点
  */
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 两种方法：正向的和逆向的
+ * 当然还可以使用栈
  */
 
 public class removeNthFromEnd {
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
-//        ListNode l2 = new ListNode(2);
-//        ListNode l3 = new ListNode(3);
-//        ListNode l4 = new ListNode(4);
-//        ListNode l5 = new ListNode(5);
-//         l1.next = l2;
-//         l2.next = l3;
-//         l3.next = l4;
-//         l4.next = l5;
-        ListNode listNode = remove(l1, 1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+         l1.next = l2;
+         l2.next = l3;
+         l3.next = l4;
+         l4.next = l5;
+        ListNode listNode = remove3(l1, 2);
 
     }
     public static ListNode remove(ListNode head, int n) {
@@ -43,7 +47,7 @@ public class removeNthFromEnd {
         p.next = q;
         return dummy.next;
     }
-    public ListNode remove2(ListNode head, int n) {
+    public static ListNode remove2(ListNode head, int n) {
         ListNode reverseNode = reverse(head);
         ListNode dummy = new ListNode();
         dummy.next = reverseNode;
@@ -55,7 +59,7 @@ public class removeNthFromEnd {
         p.next = p.next.next;
         return reverse(dummy.next);
     }
-    private ListNode reverse(ListNode head){
+    private static ListNode reverse(ListNode head){
         if(head == null || head.next == null){
             return head;
         }
@@ -70,9 +74,27 @@ public class removeNthFromEnd {
         head = p;
         return head;
     }
+    public static ListNode remove3(ListNode head, int n){
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode p = dummy;
+        LinkedList<ListNode> stack = new LinkedList<>();
+        while(p != null){
+            stack.push(p);
+            p = p.next;
+        }
+        for(int i = 0; i  < n; i++){
+            stack.pop();
+        }
+        ListNode q = stack.peek();
+        q.next = q.next.next;
+        return dummy.next;
+    }
 }
 
 /**
- * 时间复杂度:O(len)
- * 空间复杂度:o(1)
+ * 时间复杂度: O(len)
+ * 空间复杂度: O(1)
+ *
+ * 使用栈的时间复杂度和空间复杂度: O(len)，因为使用了额外空间栈
  */
